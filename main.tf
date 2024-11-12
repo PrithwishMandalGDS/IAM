37c9909dc629341af546c2fc69201fda9a5e104d
PrithwishMandalGDS <prithwishm969@gmail.com>
2024-11-12 13:00:41 +0000

    adding VPC

---
+++ main.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  source  = "app.terraform.io/AWS_IAM/MODULES/vpc"
+  version = "1.0.0"
+
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "advanced-devops-vpc"
+  }
+}
